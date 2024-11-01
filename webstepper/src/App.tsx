6d3b1ff313c44cf7ff3285e75b22385e38aaedd7
yoonieaj <144498960+yoonieaj@users.noreply.github.com>
2024-10-31 23:34:38 -0400

    Revert "Render svgs"

--- webstepper/src/App.tsx
+++ webstepper/src/App.tsx
@@ -1,37 +1,22 @@
 import React, { useState } from "react";
 import Header from "./Header";
-import { Button, Box, Typography, Stack } from "@mui/material";
-import SvgDisplay from "./SvgDisplay";
+import { Button, Box, Typography } from "@mui/material";
 
 export default function App() {
     const [step, setStep] = useState<number>(0);
-    const limit = 4;
+    // TODO: need to update the limits based on what we read from the svgs
+    const limit = 10;
     const handleStep = (newStep: number) => {
-        setStep(Math.min(Math.max(newStep, 0), limit - 1));
+        setStep(Math.min(Math.max(newStep, 0), limit));
     };
-    const svgPath = `/images/snapshot-${step}.svg`;
-
     return (
         <main className="container">
             <Header />
-            <Stack direction="row" spacing={2}>
-                <Box sx={{ width: "40%" }}>
-                    <h2>Input</h2>
-                    <Typography>Line: {step}</Typography>
-                    <Box sx={{ display: "flex" }}>
-                        <Button onClick={() => handleStep(step - 1)}>
-                            Back
-                        </Button>
-                        <Button onClick={() => handleStep(step + 1)}>
-                            Next
-                        </Button>
-                    </Box>
-                </Box>
-                <Box sx={{ width: "60%" }}>
-                    <h2>Output</h2>
-                    <SvgDisplay svgPath={svgPath} />
-                </Box>
-            </Stack>
+            <Typography>Line: {step}</Typography>
+            <Box sx={{ display: "flex" }}>
+                <Button onClick={() => handleStep(step - 1)}>Back</Button>
+                <Button onClick={() => handleStep(step + 1)}>Next</Button>
+            </Box>
         </main>
     );
 }
